b6b40e2295857b215ff4d4eb329397e078611135
ojina <oji.gogo.oji@gmail.com>
2022-07-03 01:03:45 +0900

    seeder（User,Subjects）

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+         User::create([
+            'over_name' => 'sample',
+            'under_name' => '太郎',
+            'over_name_kana' => 'サンプル',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'sample@com',
+            'sex' =>'1',
+            'birth_day' => '1995-1-1',
+            'role' => '4',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
