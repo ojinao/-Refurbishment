d41625ae7619c5cf7e5644ff5f08e3f56e0c9d7a
ojina <oji.gogo.oji@gmail.com>
2022-07-21 01:08:38 +0900

    ログインタイムアウトでlogin画面へ遷移

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('loginView');
         }
         // 改修課題：タイムアウト時の遷移先をログイン画面へ（下記に書き換える）
         // if (! $request->expectsJson()) {
