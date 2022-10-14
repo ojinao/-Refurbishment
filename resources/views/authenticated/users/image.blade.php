0a4d630fb8c66488efcc3d79f643c8dd25ca5c46
ojina <oji.gogo.oji@gmail.com>
2022-10-14 18:59:47 +0900

    微修正

--- resources/views/authenticated/users/image.blade.php
+++ resources/views/authenticated/users/image.blade.php
@@ -9,6 +9,7 @@
     <input type="submit" value="アップロード" form="imageCreate">
     <form action="{{ route('image.create') }}" method="post" id="imageCreate" enctype="multipart/form-data">{{ csrf_field() }} </form>
   </div>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7264189661537!2d139.7004369!3d35.6591115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b57efbd57c7%3A0x217e9d9fe306fba!2z5b-g54qs44OP44OB5YWs5YOP!5e0!3m2!1sja!2sjp!4v1665718609093!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
   @foreach ($imgs as $img)
   <div class="image">
@@ -18,11 +19,10 @@
       {{ csrf_field() }}
       <input type="submit" value="データ削除" onclick="return confirm('削除してよろしいですか？')">
     </form>
+    <!-- <input type="submit" value="データ削除" form="imageDelete" onclick="return confirm('削除してよろしいですか？')">
+        <form action="{{ route('image.delete', [$img->id]) }}" method="post" id="imageDelete">{{ csrf_field() }}</form>
+        <p><a href="{{ route('image.delete', ['id'=>$img->id]) }}" form="imageDelete">削除</a></p> -->
   </div>
   @endforeach
 </div>
 @endsection
-
-<!-- <input type="submit" value="データ削除" form="imageDelete" onclick="return confirm('削除してよろしいですか？')">
-    <form action="{{ route('image.delete', [$img->id]) }}" method="post" id="imageDelete">{{ csrf_field() }}</form>
-    <p><a href="{{ route('image.delete', ['id'=>$img->id]) }}" form="imageDelete">削除</a></p> -->
